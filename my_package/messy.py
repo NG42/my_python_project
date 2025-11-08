b18ed706bebf86f46efca298b0706b91444fb126
Nico Gneist <nicogneist@macbookpro.localdomain>
2025-11-08 15:09:00 +0100

    Add multiply

--- my_package/messy.py
+++ my_package/messy.py
@@ -7,3 +7,8 @@ a = 3
 def messy_function(x, y):
     """Messy initial function."""
     return x + y
+
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
--- tests/test_messy.py
+++ tests/test_messy.py
@@ -1,8 +1,14 @@
 """Testing the messy function."""
 
-from my_package.messy import messy_function
+from my_package.messy import messy_function, multiply
 
 
 def test_addition():
     """Testing addition of two numbers."""
     assert messy_function(2, 3) == 5
+
+
+def test_multiplication():
+    """Testing multiplication of two numbers."""
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
